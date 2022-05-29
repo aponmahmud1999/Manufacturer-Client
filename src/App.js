6bff8a5bc690517df1378655f07d7db28faa675e
aponmahmud1999 <aponmahmud36@gmail.com>
2022-05-29 23:09:08 +0600

    404 added

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import { Routes, Route } from "react-router-dom";
 import Header from './pages/shared/Header';
 import Footer from './pages/shared/Footer';
 import Home from './pages/home/Home';
-import Aboutus from './pages/About/Aboutus';
 import Login from './pages/Authentication/Login';
 import SignUp from './pages/Authentication/SignUp';
 import Blog from './pages/Blog/Blog';
@@ -32,7 +31,6 @@ function App() {
       <Header></Header>
       <Routes>
         <Route path='/' element={<Home></Home>} />
-        <Route path='about' element={<Aboutus></Aboutus>} />
         <Route path='blog' element={<Blog></Blog>} />
         <Route path='portfolio' element={<Portfolio></Portfolio>} />
         <Route path='login' element={<Login></Login>} />
--- src/pages/shared/Header.js
+++ src/pages/shared/Header.js
@@ -16,8 +16,7 @@ const Header = () => {
         <li><Link to='/'>Home</Link></li>
         <li><Link to='/resource'>Resource</Link></li>
         <li><Link to='/portfolio'>Portfolio</Link></li>
-        <li><Link to='/blog'>FAQ</Link></li>
-        <li><Link to='/about'>About</Link></li>
+        <li><Link to='/blog'>Blogs</Link></li>
         {
             user && <li><Link to='/dashboard'>Dashboard</Link></li>
         }
